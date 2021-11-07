2110dc5ccb70e005194cb357e33554e32419e6c6
elgar328 <cork_unsold_0k@icloud.com>
2021-11-07 17:52:19 +0000

    BOJ11727 2xn 타일링 2 (c++)

---
+++ BOJ11727/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    int n, map[1001] = {0,1,3,};
+    std::cin >> n;
+    for (int i=3; i<=n; i++)
+        map[i] = (map[i-1] + map[i-2]*2) % 10007;
+    std::cout << map[n];
+    return 0;
+}
+
