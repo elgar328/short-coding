11b10b79343d2def5013251c195ab5b6f5e06476
elgar328 <cork_unsold_0k@icloud.com>
2021-11-09 14:05:13 +0000

    BOJ11052 카드 구매하기 (c++)

---
+++ BOJ11052/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main()
+{
+    std::ios_base :: sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+
+    int p[1001], price[1001]={0,}, N;
+    std::cin >> N;
+    for (int i=1; i<=N; i++)
+        std::cin >> p[i];
+    for (int i=1; i<=N; i++)
+    {
+        price[i] = p[i];
+        for (int j=i-1; j>0; j--)
+        {
+            if (price[i] < price[j] + price[i-j])
+                price[i] = price[j] + price[i-j];
+        }
+    }
+    std::cout << price[N];
+    return 0;
+}
