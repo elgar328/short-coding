446a7c502b7b9cf9e309bfbc443affd47ff0b22b
elgar328 <cork_unsold_0k@icloud.com>
2024-03-05 11:31:12 +0900

    시험 성적

---
+++ BOJ9498/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+    let score: i32 = buf.trim().parse().unwrap();
+    println!(
+        "{}",
+        match score {
+            90..=100 => "A",
+            80..=89 => "B",
+            70..=79 => "C",
+            60..=69 => "D",
+            _ => "F",
+        }
+    );
+}
