2854954a9786fe9a6f78aadf2e7533064ca7700c
elgar328 <cork_unsold_0k@icloud.com>
2024-03-11 21:37:22 +0900

    수 정렬하기 2 (rust)

--- BOJ2751/main.rs
+++ BOJ2751/main.rs
@@ -2,7 +2,7 @@ fn main() {
     let mut num = std::io::stdin()
         .lines()
         .skip(1)
-        .flat_map(|s| s.as_ref().unwrap().trim().parse::<i32>())
+        .flat_map(|s| s.unwrap().trim().parse::<i32>())
         .collect::<Vec<i32>>();
     num.sort_unstable();
     println!(
