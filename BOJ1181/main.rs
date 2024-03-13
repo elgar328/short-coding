6221e41481c743ea717116b51fce919a125ec8f3
elgar328 <cork_unsold_0k@icloud.com>
2024-03-13 21:57:47 +0900

    단어 정렬 (rust)

--- BOJ1181/main.rs
+++ BOJ1181/main.rs
@@ -1,10 +1,7 @@
 fn main() {
-    let mut s = std::io::stdin()
-        .lines()
-        .skip(1)
-        .map(|s| s.unwrap().trim().to_owned())
-        .collect::<Vec<_>>();
-    s.sort_unstable_by_key(|s| (s.len(), s.to_owned()));
+    let buf: String = std::io::read_to_string(std::io::stdin()).unwrap();
+    let mut s: Vec<_> = buf.lines().skip(1).collect();
+    s.sort_unstable_by_key(|&s| (s.len(), s));
     s.dedup();
     println!("{}", s.join("\n"));
 }
